893db340bcc1fc85cc2dd87cdbddad76ae65603f
vini carpes <vinicdf000@gmail.com>
2024-05-29 21:58:35 -0300

    versão 3

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-'''o que devo fazer: 
-- lista de alunos que faltaram a primeira metade das aulas
-- nome e matrícula de cada aluno na lista:
-    se primeira metade == true e falta ==true exibir id_aluno
-- lista de alunos que faltaram na segunda metade da aula
-- nome e matrícula de cada aluno na lista:
-    se primeira metade == false e falta == true exibir id_aluno
-adc data em cada falta
-'''
 
 import pandas as pd #importando a biblioteca
 
@@ -20,11 +11,36 @@ faltas_coluna = planilhaProd.iloc[:,2].astype(str) #atribuindo a coluna de falta
 
 string_filtro = "True" #criando uma varável para filtrar a coluna
 
-linhas_filtradas = planilhaProd.loc[faltas_coluna.str.contains(string_filtro)] #nesta variável estou armazenando todas as colunas de cada linha em que a terceira célula seja igual a "True"
-
-print(linhas_filtradas) #exibindo a variável supracitada
+linhas_filtradas = planilhaProd.loc[faltas_coluna.str.contains(string_filtro)].astype(str) #nesta variável estou armazenando todas as colunas de cada linha em que a terceira célula seja igual a "True"
+linhas_filtradas.columns =['Situação', 'Identificação do aluno', 'Faltou?', "Data", 'Turno'] #alterando os títulos das colunas
+#print(linhas_filtradas) #exibindo a variável supracitada
 
 id_alunos_filtradas = linhas_filtradas.iloc[:,1] #aqui armazeno todas as matrículas dos faltantes
 
-print(id_alunos_filtradas) #exibindo a matrícula dos faltantes
+#print(id_alunos_filtradas) #exibindo a matrícula dos faltantes
 
+if not linhas_filtradas.empty: #verificando que não tenha posições vazias
+    for index, row in linhas_filtradas.iterrows(): #laço for que pega o índice e dados de cada linha percorrida 
+        if row["Situação"] == "atrasado":
+            linhas_filtradas.at[index, "Situação"] = "Atrasado" 
+
+if not linhas_filtradas.empty: #verificando que não tenha posições vazias
+    for index, row in linhas_filtradas.iterrows():#laço for que pega o índice e dados de cada linha percorrida 
+        if row["Faltou?"] == "True":
+            linhas_filtradas.at[index, "Faltou?"] = "Sim" 
+  
+
+if not linhas_filtradas.empty: #verificando que não tenha posições vazias
+    for index, row in linhas_filtradas.iterrows():#laço for que pega o índice e dados de cada linha percorrida 
+        if row["Turno"] == "True":
+            linhas_filtradas.at[index, "Turno"] = "18h - 20h" 
+        else: linhas_filtradas.at[index, "Turno"] = "20h - 22h" 
+
+print(linhas_filtradas)
+
+planilhaAlunos = pd.read_csv('ID Alunos.csv').astype(str)
+#print(planilhaAlunos)
+
+id_to_name = pd.Series(planilhaAlunos['nome'].values, index=planilhaAlunos["id"]).to_dict() #Criar um dicionário onde as chaves são os IDs dos alunos e os valores são os nomes dos alunos:
+linhas_filtradas['Nome do Aluno'] = linhas_filtradas['Identificação do aluno'].map(id_to_name) 
+print(linhas_filtradas)
